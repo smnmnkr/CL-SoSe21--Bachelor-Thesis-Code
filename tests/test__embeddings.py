f82d1b3bce540cb50e7eabf1cb329a47dca91e1f
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2020-10-12 21:33:47 +0200

    refractored fasttext tests

--- tests/test__embeddings.py
+++ tests/test__embeddings.py
@@ -1,3 +1,4 @@
+import pytest
 import torch
 
 from geneticNLP.embeddings import FastText
@@ -20,25 +21,35 @@ test_sent: list = [
 ]
 
 
-def test_FastText():
+# share fixed FastText object across module test
+# src: https://docs.pytest.org/en/2.8.7/fixture.html
+@pytest.fixture(scope="module")
+def fs_fixture():
+    return FastText(fasttext_file)
 
-    # create embedding object
-    embedder = FastText(fasttext_file)
+
+def test_FastText_init(fs_fixture):
 
     # check dimensionality of the embeddings
-    assert embedder.embedding_dim() == fasttext_dim
+    assert fs_fixture.embedding_dim() == fasttext_dim
 
     # check dimensionality of the embeddings
-    assert embedder.embedding_dim() == fasttext_dim
+    assert fs_fixture.embedding_dim() == fasttext_dim
+
+
+def test_FastText_forward_word(fs_fixture):
 
     # test: forward single word
-    forward_word = embedder.forward(test_word)
+    forward_word = fs_fixture.forward(test_word)
 
     assert isinstance(forward_word, torch.FloatTensor)
     assert forward_word.size()[0] == fasttext_dim
 
+
+def test_FastText_fordward_sentence(fs_fixture):
+
     # test: forward multiply words/sentence
-    forward_sent = embedder.forwards(test_sent)
+    forward_sent = fs_fixture.forwards(test_sent)
 
     assert isinstance(forward_sent, torch.FloatTensor)
     assert forward_sent.size()[0] == len(test_sent)
