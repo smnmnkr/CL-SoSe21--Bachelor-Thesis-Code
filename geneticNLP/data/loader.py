719ca2dc1aecba1f581ecf4d7d773ad374c7bc1f
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2020-10-13 10:04:17 +0200

    updated batch_loader

--- geneticNLP/data/loader.py
+++ geneticNLP/data/loader.py
@@ -8,12 +8,17 @@ from torch.utils.data import Dataset, IterableDataset, DataLoader
 #
 def batch_loader(
     data_set: Union[IterableDataset, Dataset],
-    batch_size: bool,
-    shuffle=False,
+    batch_size: int = 32,
+    shuffle: bool = False,
+    num_workers: int = 10,
 ) -> DataLoader:
+    """
+    Create a batch data loader from the given data set.
+    """
     return DataLoader(
         data_set,
         batch_size=batch_size,
         collate_fn=lambda x: x,
         shuffle=shuffle,
+        num_workers=num_workers,
     )
