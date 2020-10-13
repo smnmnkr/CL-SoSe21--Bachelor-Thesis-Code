6897178405de846b1acd132e0bd14ab24a2c86a0
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2020-10-13 20:33:15 +0200

    mutate via hadermard product instead of parameterwise

--- geneticNLP/neural/ga/mutation.py
+++ geneticNLP/neural/ga/mutation.py
@@ -1,6 +1,8 @@
 import random
 import copy
 
+import torch
+
 #
 #
 #  -------- mutate -----------
@@ -10,30 +12,9 @@ def mutate(
     mutation_rate: float = 0.02,
 ):
 
-    #  -------- _mutate -----------
-    #
-    def _mutate(param):
-        return param + (mutation_rate * random.randint(-1, 1))
-
     child_network = copy.deepcopy(parent_network)
 
     for layer in child_network.parameters():
-
-        if len(layer.shape) == 4:
-            print("shape not mutated")
-
-        if len(layer.shape) == 3:
-            print("shape not mutated")
-
-        # mutate matrices : [1 x n]
-        if len(layer.shape) == 2:
-            for i0 in range(layer.shape[0]):
-                for i1 in range(layer.shape[1]):
-                    layer[i0][i1] = _mutate(layer[i0][i1])
-
-        # mutate vectors : [1]
-        if len(layer.shape) == 1:
-            for i0 in range(layer.shape[0]):
-                layer[i0] = _mutate(layer[i0])
+        layer = layer + torch.rand(tuple(layer.shape))  # * mutation_rate
 
     return child_network
