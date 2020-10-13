799205d1f18ce00f47faa49891b39e3abf6efc76
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2020-10-13 18:33:38 +0200

    switched to evolution training

--- geneticNLP/__main__.py
+++ geneticNLP/__main__.py
@@ -1,8 +1,8 @@
 from geneticNLP.data import CONLLU
-from geneticNLP.embeddings import FastText
+from geneticNLP.embeddings import FastText, Untrained
 from geneticNLP.models.postagger import POSTagger, batch_loss, accuracy
 
-from geneticNLP.neural import train
+from geneticNLP.neural import train, evolve
 
 from geneticNLP.utils import Encoding
 
@@ -14,25 +14,35 @@ data_dev = CONLLU("./data/universal-dependencies--en-test.conllu")
 model = POSTagger(
     {
         "lstm": {
-            "hidden_size": 50,
+            "hidden_size": 24,
             "depth": 2,
             "dropout": 0.5,
         },
         "score": {
-            "hidden_size": 50,
+            "hidden_size": 24,
             "dropout": 0.5,
         },
     },
     Encoding({tok.pos for sent in data_train for tok in sent}),
-    FastText("./data/fasttext--cc.en.300.bin"),
+    Untrained({tok.word for sent in data_train for tok in sent}, 36),
+    # FastText("./data/fasttext--cc.en.300.bin"),
 )
 
 
-train(
+evolve(
     model,
     data_train,
     data_dev,
-    batch_loss,
     accuracy,
-    report_rate=10,
+    epoch_num=6,
+    report_rate=2,
 )
+
+# train(
+#     model,
+#     data_train,
+#     data_dev,
+#     batch_loss,
+#     accuracy,
+#     report_rate=10,
+# )
