84e96bb625fc488ac0cd1e7a5c52be77e00eccd9
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2020-10-21 17:05:40 +0200

    removed fitness bias in mutation

--- geneticNLP/neural/ga/mutation.py
+++ geneticNLP/neural/ga/mutation.py
@@ -8,15 +8,12 @@ import torch
 @torch.no_grad()
 def mutate(
     parent_network,
-    score: float = 0.0,
     mutation_rate: float = 0.02,
 ):
 
     child_network = copy.deepcopy(parent_network)
 
     for param in child_network.parameters():
-        param.data += (
-            mutation_rate * torch.randn_like(param) * (1.0 - score)
-        )
+        param.data += mutation_rate * torch.randn_like(param)
 
     return child_network
