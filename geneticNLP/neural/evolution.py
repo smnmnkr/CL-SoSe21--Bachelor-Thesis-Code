4cf709258b1d5678f9a617b644af3d15f5a2492e
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2020-10-29 23:38:08 +0100

    replaced convergence minimum with epoch num

--- geneticNLP/neural/evolution.py
+++ geneticNLP/neural/evolution.py
@@ -21,10 +21,10 @@ def evolve(
     model: nn.Module,
     train_set: IterableDataset,
     dev_set: IterableDataset,
-    convergence_min: int = 0.8,
     population_size: int = 80,
     selection_rate: int = 10,
     crossover_rate: float = 0.5,
+    epoch_num: int = 200,
     report_rate: int = 10,
     batch_size: int = 32,
 ):
@@ -38,14 +38,10 @@ def evolve(
         num_workers=0,
     )
 
-    # start convergence, epoch, population
-    convergence: float = 0.0
-    epoch: int = 0
     population: dict = {model: model.evaluate(dev_loader)}
 
     # --
-    while convergence < convergence_min:
-        epoch += 1
+    for epoch in range(1, epoch_num + 1):
         time_begin = datetime.now()
 
         # load train set as batched loader
@@ -66,18 +62,17 @@ def evolve(
             )
 
         # --- report
-        if (epoch + 1) % report_rate == 0:
+        if epoch % report_rate == 0:
 
             # --- evaluate all models on train set
             evaluate_parallel(population, train_loader)
 
             # --- find best model and corresponding score
             best, score = dict_max(population)
-            convergence = score
 
             print(
                 "[--- @{:02}: \t avg(train)={:2.4f} \t best(train)={:2.4f} \t best(dev)={:2.4f} \t time(epoch)={} ---]".format(
-                    (epoch + 1),
+                    epoch,
                     sum(population.values()) / len(population),
                     score,
                     best.evaluate(dev_loader),
--- geneticNLP/neural/swarm.py
+++ geneticNLP/neural/swarm.py
@@ -22,10 +22,10 @@ def swarm(
     dev_set: IterableDataset,
     noise_std: float = 0.1,
     learning_rate: float = 0.001,
-    convergence_min: int = 0.8,
     population_size: int = 80,
     selection_rate: float = 10,
     crossover_rate: float = 0.5,
+    epoch_num: int = 200,
     report_rate: int = 10,
     batch_size: int = 32,
 ):
@@ -38,17 +38,12 @@ def swarm(
         batch_size=batch_size,
     )
 
-    # start convergence, epoch
-    convergence: float = 0.0
-    epoch: int = 0
-
     # generate queen, swarm
     queen: nn.Module = model
     population: dict = {queen: queen.evaluate(dev_loader)}
 
     # --
-    while convergence < convergence_min:
-        epoch += 1
+    for epoch in range(1, epoch_num + 1):
         time_begin = datetime.now()
 
         # load train set as batched loader
@@ -78,7 +73,6 @@ def swarm(
 
         # --- report
         if epoch % report_rate == 0:
-            convergence = queen.evaluate(train_loader)
 
             # --- evaluate all models on train set
             evaluate_parallel(population, train_loader)
@@ -87,7 +81,7 @@ def swarm(
                 "[--- @{:02}: \t avg(train)={:2.4f} \t queen(train)={:2.4f} \t queen(dev)={:2.4f} \t time(epoch)={} ---]".format(
                     epoch,
                     sum(population.values()) / len(population),
-                    convergence,
+                    queen.evaluate(train_loader),
                     queen.evaluate(dev_loader),
                     datetime.now() - time_begin,
                 )
