d7a67a39b95b80bf02ae3f43ad0796b75e4d8efc
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2021-03-29 21:19:15 +0200

    enhance gadam

--- beyondGD/optimizer/gadam.py
+++ beyondGD/optimizer/gadam.py
@@ -10,7 +10,7 @@ from beyondGD.optimizer.evolution import elitism, mutate, crossover
 
 from beyondGD.optimizer.util import (
     evaluate_on_loader,
-    accuracy_on_batch,
+    # accuracy_on_batch,
     get_rnd_entity,
     get_rnd_prob,
 )
@@ -48,14 +48,23 @@ def gadam(
         batch_size=batch_size,
     )
 
+    # load dev set as batched loader
+    dev_loader: DataLoader = batch_loader(
+        dev_set,
+        batch_size=batch_size,
+        num_workers=0,
+    )
+
     # --
     for epoch in range(1, epoch_num + 1):
         time_begin: datetime = datetime.now()
 
         for batch in train_loader:
 
+            population: dict = evaluate_on_loader(population, dev_loader)
+
             # --- calculate accuracy on batch
-            population: dict = accuracy_on_batch(population, batch)
+            # population: dict = accuracy_on_batch(population, batch)
 
             # --- select by elite
             selection: dict = elitism(population, selection_size)
@@ -66,8 +75,9 @@ def gadam(
             # --- fill new population with mutated, crossed entities
             for _ in range(population_size):
 
-                # get random players from selection
+                # get random players from selection, disable dropout
                 entity: Module = get_rnd_entity(selection)
+                entity.train(False)
 
                 # (optionally) cross random players
                 if crossover_prob > get_rnd_prob():
@@ -78,6 +88,9 @@ def gadam(
                 # (optionally) mutate random players
                 if mutation_prob > get_rnd_prob():
                     entity: Module = mutate(entity, mutation_rate)
+
+                # enable dropout
+                entity.train(True)
 
                 # choose Adam for optimization
                 # https://pytorch.org/docs/stable/optim.html#torch.optim.Adam
@@ -106,24 +119,17 @@ def gadam(
         if epoch % report_rate == 0:
 
             # --- evaluate all models on train set
-            population: dict = evaluate_on_loader(population, train_loader)
+            population: dict = evaluate_on_loader(population, dev_loader)
 
             # --- find best model and corresponding score
-            best, train_score = dict_max(population)
-
-            # load dev set as batched loader
-            dev_loader: DataLoader = batch_loader(
-                dev_set,
-                batch_size=batch_size,
-                num_workers=0,
-            )
+            best, dev_score = dict_max(population)
 
             print(
                 "[--- @{:02}: \t avg(train)={:2.4f} \t best(train)={:2.4f} \t best(dev)={:2.4f} \t time(epoch)={} ---]".format(
                     epoch,
                     sum(population.values()) / len(population),
-                    train_score,
-                    best.evaluate(dev_loader),
+                    best.evaluate(train_loader),
+                    dev_score,
                     datetime.now() - time_begin,
                 )
             )
