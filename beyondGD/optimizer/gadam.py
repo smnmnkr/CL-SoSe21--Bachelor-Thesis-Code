7fffd9f7552cb1fc35baf796f4ac165a101c0e93
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2021-03-29 18:45:22 +0200

    import genetic operators from evolve

--- beyondGD/optimizer/gadam.py
+++ beyondGD/optimizer/gadam.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-import itertools
 
 import torch
 from torch.optim import Adam
@@ -7,15 +6,15 @@ from torch.optim import Adam
 from beyondGD.data import batch_loader
 from beyondGD.utils import dict_max
 
+from beyondGD.optimizer.evolution import elitism, crossover
+
 from beyondGD.optimizer.util import (
     evaluate_on_loader,
     accuracy_on_batch,
     get_rnd_entity,
     get_rnd_prob,
-    copy_model,
 )
 
-from beyondGD.utils import get_device
 from beyondGD.utils.type import TT, IterableDataset, Module, DataLoader
 
 
@@ -125,51 +124,3 @@ def gadam(
             )
 
     return population
-
-
-#
-#
-#  -------- elitism -----------
-#
-def elitism(generation: dict, n: int) -> dict:
-
-    ranking: dict = {
-        k: v
-        for k, v in sorted(generation.items(), key=lambda item: item[1])
-    }
-
-    return dict(itertools.islice(ranking.items(), len(ranking) - n, None))
-
-
-#
-#
-#  -------- crossover -----------
-#
-@torch.no_grad()
-def crossover(
-    dom_network: Module,
-    rec_network: Module,
-    dominance: float = 0.5,
-) -> Module:
-
-    child_network: Module = copy_model(dom_network)
-
-    for c_layer, r_layer in zip(
-        child_network.parameters(),
-        rec_network.parameters(),
-    ):
-
-        # create mask, which determines the retained weights
-        mask_positive: TT = (
-            (torch.FloatTensor(c_layer.shape).uniform_() > dominance)
-            .int()
-            .to(get_device())
-        )
-
-        # create inverted mask
-        mask_negativ: TT = torch.abs(mask_positive - 1).to(get_device())
-
-        # combine the two layers
-        c_layer.data = c_layer * mask_positive + r_layer * mask_negativ
-
-    return child_network
